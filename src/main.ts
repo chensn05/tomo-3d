a85aa88343d964e8c9c6e8c6b149898bd88e42b9
chensn05 <chensn05@users.noreply.github.com>
2026-08-31 23:20:21 +0800

    🐛 restore hash router (GitHub Pages)

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { createApp } from 'vue'
 import TomoIcon from './components/TomoIcon.vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
 import './style.css'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     { path: '/', component: () => import('./pages/Home.vue') },
     { path: '/story', component: () => import('./pages/Story.vue') },
